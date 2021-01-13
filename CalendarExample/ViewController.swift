ff86d8a783a610751116149cc0b6196960b61a46
SebaKrk <s.sciuba@gmail.com>
2021-01-13 09:08:54 +0100

    added EKEvent to handleAddButton func

--- CalendarExample/ViewController.swift
+++ CalendarExample/ViewController.swift
@@ -26,6 +26,26 @@ class ViewController: UIViewController {
     }
     @objc func handleAddButton() {
         print("add")
+        
+        eventStore.requestAccess( to: EKEntityType.event, completion:{(granted, error) in
+            DispatchQueue.main.async {
+                if (granted) && (error == nil) {
+                    let event = EKEvent(eventStore: self.eventStore)
+                    event.title = "Keynote Apple"
+                    event.startDate = self.time
+                    event.url = URL(string: "https://apple.com")
+                    event.endDate = self.time
+
+                    let eventController = EKEventEditViewController()
+                    eventController.event = event
+                    eventController.eventStore = self.eventStore
+                    eventController.editViewDelegate = self
+
+                    self.present(eventController, animated: true, completion: nil)
+
+                }
+            }
+        })
     }
     
 }
