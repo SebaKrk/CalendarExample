a605ab226b13b43b257ce85114b077969f041af6
SebaKrk <s.sciuba@gmail.com>
2021-01-13 09:00:54 +0100

    create rootviewcontroller

--- CalendarExample/ViewController.swift
+++ CalendarExample/ViewController.swift
@@ -17,25 +17,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        eventStore.requestAccess( to: EKEntityType.event, completion:{(granted, error) in
-            DispatchQueue.main.async {
-                if (granted) && (error == nil) {
-                    let event = EKEvent(eventStore: self.eventStore)
-                    event.title = "Keynote Apple"
-                    event.startDate = self.time
-                    event.url = URL(string: "https://apple.com")
-                    event.endDate = self.time
-                    
-                    let eventController = EKEventEditViewController()
-                    eventController.event = event
-                    eventController.eventStore = self.eventStore
-                    eventController.editViewDelegate = self
-                    
-                    self.present(eventController, animated: true, completion: nil)
-                    
-                }
-            }
-        })
+        view.backgroundColor = .red
+        
+//        eventStore.requestAccess( to: EKEntityType.event, completion:{(granted, error) in
+//            DispatchQueue.main.async {
+//                if (granted) && (error == nil) {
+//                    let event = EKEvent(eventStore: self.eventStore)
+//                    event.title = "Keynote Apple"
+//                    event.startDate = self.time
+//                    event.url = URL(string: "https://apple.com")
+//                    event.endDate = self.time
+//
+//                    let eventController = EKEventEditViewController()
+//                    eventController.event = event
+//                    eventController.eventStore = self.eventStore
+//                    eventController.editViewDelegate = self
+//
+//                    self.present(eventController, animated: true, completion: nil)
+//
+//                }
+//            }
+//        })
         
     }
     
