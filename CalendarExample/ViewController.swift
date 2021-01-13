f768c0c1edc831d5536a53ae3a1170f7995f681f
SebaKrk <s.sciuba@gmail.com>
2021-01-13 08:54:54 +0100

    add an event to our calendar func

--- CalendarExample/ViewController.swift
+++ CalendarExample/ViewController.swift
@@ -11,9 +11,29 @@ import EventKitUI
 
 class ViewController: UIViewController {
     
+    let eventStore = EKEventStore()
+    var time = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        eventStore.requestAccess( to: EKEntityType.event, completion:{(granted, error) in
+            DispatchQueue.main.async {
+                if (granted) && (error == nil) {
+                    let event = EKEvent(eventStore: self.eventStore)
+                    event.title = "Keynote Apple"
+                    event.startDate = self.time
+                    event.url = URL(string: "https://apple.com")
+                    event.endDate = self.time
+                    let eventController = EKEventEditViewController()
+                    eventController.event = event
+                    eventController.eventStore = self.eventStore
+                    eventController.editViewDelegate = self
+                    self.present(eventController, animated: true, completion: nil)
+                    
+                }
+            }
+        })
         
     }
     
@@ -21,6 +41,10 @@ class ViewController: UIViewController {
 extension ViewController: EKEventEditViewDelegate {
     
     func eventEditViewController(_ controller: EKEventEditViewController, didCompleteWith action: EKEventEditViewAction) {
-        <#code#>
+        
+        controller.dismiss(animated: true, completion: nil)
     }
 }
+
+
+//clone - https://medium.com/swift-productions/add-an-event-to-the-calendar-xcode-12-swift-5-3-35b8bf149859
